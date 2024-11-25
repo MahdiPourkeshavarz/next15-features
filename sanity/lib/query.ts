f003c3c380d236b59e86f2636363af110e98936d
mahdi keshavarz <69865082+ma-pk1996@users.noreply.github.com>
2024-11-25 18:56:10 +0330

    feat(startupPage): ui and logic completed. also used ppr feature for this page

--- sanity/lib/query.ts
+++ sanity/lib/query.ts
@@ -12,5 +12,57 @@ export const STARTUPS_QUERY =
   views,
   description,
   category,
-  image
+  image,
+  pitch
   }`);
+
+export const STARTUP_ID_QUERY =
+  defineQuery(`*[_type == "startup" & _id == $id] [0] {
+  _id,
+    title,
+    slug,
+    _createdAt,
+    author -> {
+      _id,
+      name,
+      username,
+      image,
+      bio
+    },
+    views,
+    description,
+    category,
+    image,
+    pitch
+}`);
+
+export const STARTUP_VIEWS_QUERY = defineQuery(`
+  *[_type == "startup" && _id == $id][0]{
+    _id, views
+  }
+  `);
+
+export const PLAYLIST_BY_SLUG_QUERY =
+  defineQuery(`*[_type == "playlist" && slug.current == $slug][0]{
+  _id,
+  title,
+  slug,
+  select[]->{
+    _id,
+    _createdAt,
+    title,
+    slug,
+    author->{
+      _id,
+      name,
+      slug,
+      image,
+      bio
+    },
+    views,
+    description,
+    category,
+    image,
+    pitch
+  }
+}`);
